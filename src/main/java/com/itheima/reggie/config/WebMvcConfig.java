61af7ba1745f402a5da1a8bdbc99fdcb26888da2
wuliuye <wuliuye@163.com>
2022-05-21 16:25:28 +0800

    解决雪花算法Id精度在丢失导致更新员工信息失败的bug

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.reggie.config;
 
+import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @author : wly
@@ -22,6 +27,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
      * "classpath:/public/"
      * springboot默认可以访问静态资源的目录下，就不用设置这个
      * 正常情况下不用设置这个
+     *
      * @param registry
      * @return void
      **/
@@ -30,5 +36,22 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始进行静态资源映射。。。。");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     *
+     * @param converters
+     * @return void
+     **/
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转换Json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器追加到mvc框架的转换器集合中，并排在第一位
+        converters.add(0, messageConverter);
     }
 }
