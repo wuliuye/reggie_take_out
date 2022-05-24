962669bc304b761e5b441e0f45eeb393c4571f40
wuliuye <wuliuye@163.com>
2022-05-24 15:52:18 +0800

    feature: 添加购物车相关功能
    添加查看用户购物车商品功能
    添加清空购物车功能
    添加减少购物车商品数量功能

--- src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -8,12 +8,10 @@ import com.itheima.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author : wly
@@ -78,5 +76,88 @@ public class ShoppingCartController {
         }
 
         return R.success(cartServiceOne);
+    }
+
+
+    /**
+     * 或者用户的购物车列表
+     *
+     * @param
+     * @return com.itheima.reggie.common.R<java.util.List < com.itheima.reggie.entity.ShoppingCart>>
+     **/
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+
+        log.info("查看购物车...");
+        //获取用户Id
+        Long userId = BaseContext.getCurrentId();
+        //查询购物车数据
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(userId != null, ShoppingCart::getUserId, userId);
+        queryWrapper.orderByAsc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    /**
+     * 减少购物车内商品数量
+     *
+     * @param shoppingCart
+     * @return com.itheima.reggie.common.R<java.lang.String>
+     **/
+    @PostMapping("/sub")
+    public R<String> sub(@RequestBody ShoppingCart shoppingCart) {
+
+        //设置用户id，指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        Long dishId = shoppingCart.getDishId();
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+
+        //要减少的是菜品
+        if (dishId != null) {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+
+        } else {
+            //要减少的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+
+        //查询要减少的菜品或者套餐在数据库的数量
+        //SQL:select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        //如果大于一件就减少数量
+        if (cartServiceOne.getNumber() > 1) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number - 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            //如果数据库只有一件该商品，则直接删除
+            shoppingCartService.remove(queryWrapper);
+        }
+        
+        return R.success("减少商品成功");
+    }
+
+    /**
+     * 清空购物车
+     *
+     * @param
+     * @return com.itheima.reggie.common.R<java.lang.String>
+     **/
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+        //SQL:delete from shopping_cart where user_id = ?
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+
+        shoppingCartService.remove(queryWrapper);
+
+        return R.success("清空购物车成功");
     }
 }
